08c79e8ec62a528638ad9329163a51f063086edd
Alexander Kraus <nr4@z10.info>
2020-05-23 17:55:51 +0200

    added rudimentary websocket client implementation

--- hardware-interface-client/index.js
+++ hardware-interface-client/index.js
@@ -1,14 +1,43 @@
 const midi = require('midi');
+const fs = require('fs');
+const path = require('path');
+const ParseMidi = require('midi-file').parseMidi;
+const WebSocketClient = require('websocket').client;
 
-const input = new midi.input(),
-    output = new midi.output();
-var numberOfInputDevices = input.getPortCount(),
-    numberOfOutputDevices = output.getPortCount();
-for(var i=0; i<numberOfInputDevices; ++i)
+const midiOutput = new midi.output();
+
+// List available input and output devices
+function listAvailableOutputDevices()
 {
-    console.log("Midi input port", i, "has device", input.getPortName(i));
+    var numberOfDevices = midiOutput.getPortCount(),
+        availableDevices = {};
+
+    for(var i = 0; i < numberOfDevices; ++i)
+        availableDevices[midiOutput.getPortName(i)] = i;
+    return availableDevices;
 }
-for(var i=0; i<numberOfOutputDevices; ++i)
-{
-    console.log("Midi output port", i, "has device", output.getPortName(i));
-}
+
+// Set up websocket client
+configuration = JSON.parse(fs.readFileSync('config.json'));
+var client = new WebSocketClient();
+
+client.on('connectFailed', (error) => {
+    console.error('Failed to connect; error message: ', error.toString());
+});
+
+client.on('connect', (connection) => {
+    console.log('Connected.');
+    connection.on('error', (error) => {
+        console.log('Connection error; error message: ', error.toString());
+    });
+    connection.on('close', () => {
+        console.log('Connection closed.');
+    });
+    connection.on('message', (message) => {
+        console.log('Connection message: ', message);
+    });
+
+    clientData = configuration.client;
+});
+
+client.connect(configuration.server.address, 'cultProtocol');
